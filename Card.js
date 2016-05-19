e7f460592b5762b7388d4d8409eba04360b021f0
Nelson Rodriguez <nelsao101@gmail.com>
2016-05-19 14:33:17 +0200

    Adding state and event to Card component for showing tasks

--- Card.js
+++ Card.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 class Card extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      showDetails: false,
+    }
+  }
+
+  showDetails(event){
+    event.preventDefault();
+    this.setState({showDetails: !this.state.showDetails})
+  }
+
   render(){
+    let cardDetais = this.state.showDetails ?
+    <div className="card__details">
+      {this.props.description}
+      <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+    </div> : '';
+
     return(
       <div className="card">
-        <div className="card__title">{this.props.title}</div>
-        <div className="card__details">
-          {this.props.description}
-          <CheckList cardId={this.props.id} tasks={this.props.tasks} />
-        </div>
+        <div className="card__title" onClick={this.showDetails.bind(this)} >{this.props.title}</div>
+        {cardDetais}
       </div>
     );
   }
